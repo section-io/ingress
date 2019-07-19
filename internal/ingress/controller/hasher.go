607fd012ae60c4cda7c472a9c7aea2a02f8c6d9d
Matthew Wickman <mattw@section.io>
2019-07-19 16:10:29 -0600

    replacing the existing hash function with a simpler and more correct function which runs many times faster. This speeds up reloads significantly, 12 min to 1 sec in some clusters

---
+++ internal/ingress/controller/hasher.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"hash/fnv"
+	"k8s.io/ingress-nginx/internal/ingress"
+)
+
+// HashConfig Creates a hash of the Configuration using fnv
+func HashConfig(config ingress.Configuration) uint64 {
+	hash := fnv.New64()
+
+	for _, b := range config.Backends {
+		hash.Write([]byte(b.Name))
+	}
+
+	for _, s := range config.Servers {
+		hash.Write([]byte(s.Hostname))
+	}
+
+	return hash.Sum64()
+}
